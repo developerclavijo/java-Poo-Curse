647761d8b04c0b68bb091f5f85c06a442c1c0b67
Clavijo <dev.ronny@outlook.com>
2021-01-16 21:23:53 -0500

    Agrego codigo

---
+++ src/principal/PrimeraClase.java
@@ -0,0 +1,9 @@
+package principal;
+
+public class PrimeraClase {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, soy un mensaje normal");
+        System.err.println("Hola, soy un mensaje de error");
+    }
+}
