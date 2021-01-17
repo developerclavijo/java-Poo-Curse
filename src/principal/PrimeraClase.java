51db3855079991e6e2a55bba03dab850ce65ae5b
Clavijo <dev.ronny@outlook.com>
2021-01-16 21:55:13 -0500

    Agregar

--- src/principal/PrimeraClase.java
+++ src/principal/PrimeraClase.java
@@ -1,9 +1,17 @@
 package principal;
 
 public class PrimeraClase {
-
+    //Este es un comentario de una linea
+    /*
+    Este
+    Es
+    Un
+    Comentario
+    Largo
+    */
     public static void main(String[] args) {
-        System.out.println("Hola, soy un mensaje normal");
-        System.err.println("Hola, soy un mensaje de error");
+
+        System.out.println("Hola, Soy Ronny \ny seré tu instructor \tde este curso :)");
+        System.err.println("Espero \nPongas \tEmpeño");
     }
 }
