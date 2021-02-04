62f3805a12ed43a395f7c079c91b2b966a4d6518
Ronny Clavijo <dev.ronny@gmail.com>
2021-02-03 20:54:39 -0500

    bucle for

---
+++ src/EstructurasDeControl/EstructuraFor.java
@@ -0,0 +1,12 @@
+package EstructurasDeControl;
+
+public class EstructuraFor {
+    public static void main(String[] args) {
+        String letra = "Hola";
+        //Bucle for
+        for (int i = 0; i < 10; i++) {
+            System.out.println("");
+            letra = "Hola";
+        }
+    }
+}
--- src/EstructurasDeControl/StructuraDo_While.java
+++ src/EstructurasDeControl/StructuraDo_While.java
@@ -4,12 +4,17 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 public class StructuraDo_While {
-    static int i = 1, tablaNumero = 0, terminaTablaMultiplicar = 0, resultado = 0;
+    static int i = 1, tablaNumero = 0, terminaTablaMultiplicar = 0, resultado = 0, edad = 0;
+    static String respuesta = "";
     static Scanner leer = new Scanner(System.in);
 
     public static void main(String[] args) {
         //Bucles
         //While - do while - for - forEach
+
+        /*
+            Haz una fiesta, pero que solo puedan ingresar personas mayores a 18 años.
+         */
 
         /*System.out.println("¿Cual tabla de multiplicar quieres sacar?");
         tablaNumero = Integer.parseInt(leer.nextLine());
@@ -23,13 +28,51 @@ public class StructuraDo_While {
             System.out.println(tablaNumero + "*" + i + "= " + resultado);
             i++;
         }*/
-        i = 0;
+        /*i = 1;
 
         do {
+            System.out.println(i);
             i++;
+        } while (i == 2);*/
 
-            System.out.println("Hola, soy: " + i);
-            System.out.println("XD");
-        } while (i == 0);
+        /*System.out.println("¿Alguien va a intentar ingresar?" +
+                "\nSI o NO");
+        respuesta = leer.nextLine();*/
+
+        /*while (respuesta.equalsIgnoreCase("SI")) {
+            System.out.println("¿Cual es tu edad?");
+            edad = Integer.parseInt(leer.nextLine());
+
+            if (edad >= 18) {
+                System.out.println("Puedes pasar");
+            } else {
+                System.out.println("No puedes pasar");
+            }
+
+            System.out.println("¿Alguien va a intentar ingresar?" +
+                    "\nSI o NO");
+            respuesta = leer.nextLine();
+        }*/
+
+        do {
+            System.out.println("¿Alguien va a intentar ingresar?" +
+                    "\nSI o NO");
+            respuesta = leer.nextLine();
+
+            if (!respuesta.equalsIgnoreCase("NO")) {
+                System.out.println("¿Cual es tu edad?");
+                edad = Integer.parseInt(leer.nextLine());
+
+                if (edad >= 18) {
+                    System.out.println("Puedes pasar");
+                } else {
+                    System.out.println("No puedes pasar");
+                }
+            } else {
+                break;
+            }
+
+        } while (respuesta.equalsIgnoreCase("SI"));
+
     }
 }
